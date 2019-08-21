34e2ea3142b7e2c703a5375106a4121c8ca0dabb
GabrielBergamim <gabriel.bergamim@outlook.com>
2019-08-21 00:07:51 -0300

    FM, I'm in hell

---
+++ aulajpa/src/aplicacao/Programa.java
@@ -0,0 +1,39 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		/*Pessoa p1 = new Pessoa(null, "Gabriel", "gabriel@aluno.com.br");
+		Pessoa p2 = new Pessoa(null, "Caio", "caio@aluno.com.br");
+		Pessoa p3 = new Pessoa(null, "Fabio", "fabio@aluno.com.br");*/
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		/*em.getTransaction().begin();
+		
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		
+		em.getTransaction().commit();*/
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		/*em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();*/
+		System.out.println(p);
+		
+		em.close();
+		emf.close();
+		
+	}
+
+}
