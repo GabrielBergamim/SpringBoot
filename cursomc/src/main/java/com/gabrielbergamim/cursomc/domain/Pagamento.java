f7f27f3797d506a8cb93c585633a8ffc5ef8ebaf
GabrielBergamim <gabriel.bergamim@outlook.com>
2019-09-06 17:38:57 -0300

    1
    JSonIgnore

--- cursomc/src/main/java/com/gabrielbergamim/cursomc/domain/Pagamento.java
+++ cursomc/src/main/java/com/gabrielbergamim/cursomc/domain/Pagamento.java
@@ -12,7 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gabrielbergamim.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -28,7 +28,7 @@ public abstract class Pagamento implements Serializable{
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
-	@JsonBackReference
+	@JsonIgnore
 	private Pedido pedido;
 	
 	public Pagamento() {
