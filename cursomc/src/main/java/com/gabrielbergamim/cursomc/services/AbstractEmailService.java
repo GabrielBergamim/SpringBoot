d80276b565495aef39c67f76e335a2668f74d73a
GabrielBergamim <gabriel.bergamim@outlook.com>
2019-09-12 22:23:18 -0300

    Email funcionando!!

--- cursomc/src/main/java/com/gabrielbergamim/cursomc/services/AbstractEmailService.java
+++ cursomc/src/main/java/com/gabrielbergamim/cursomc/services/AbstractEmailService.java
@@ -2,15 +2,29 @@ package com.gabrielbergamim.cursomc.services;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 
 import com.gabrielbergamim.cursomc.domain.Pedido;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 public abstract class AbstractEmailService implements EmailService{
 	
 	@Value("${default.sender}")
 	private String sender;
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	@Autowired
+	private TemplateEngine templateEngine;
 	
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
@@ -28,4 +42,38 @@ public abstract class AbstractEmailService implements EmailService{
 		
 		return sm;
 	}
+
+	protected String htmlFromTemplatePedido(Pedido obj) {
+		Context context = new Context();
+
+		context.setVariable("pedido", obj);
+		return templateEngine.process("email/confirmacaoPedido", context);
+	}
+
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj){
+		MimeMessage mm = null;
+		try {
+			mm = prepareMimeMessageFromPedido(obj);
+			sendHtmlEmail(mm);
+		} catch (MessagingException e) {
+			sendOrderConfirmationEmail(obj);
+		}
+
+	}
+
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+		mimeMessageHelper.setTo(obj.getCliente().getEmail());
+		mimeMessageHelper.setFrom(sender);
+		mimeMessageHelper.setSubject("Pedido confirmado! Código: "+obj.getId());
+		mimeMessageHelper.setSentDate(new Date(System.currentTimeMillis()));
+		mimeMessageHelper.setText(htmlFromTemplatePedido(obj), true);
+
+		return mimeMessage;
+	}
+
+
 }
--- cursomc/src/main/java/com/gabrielbergamim/cursomc/services/EmailService.java
+++ cursomc/src/main/java/com/gabrielbergamim/cursomc/services/EmailService.java
@@ -4,10 +4,16 @@ import org.springframework.mail.SimpleMailMessage;
 
 import com.gabrielbergamim.cursomc.domain.Pedido;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendHtmlEmail(MimeMessage msg);
 	
 }
