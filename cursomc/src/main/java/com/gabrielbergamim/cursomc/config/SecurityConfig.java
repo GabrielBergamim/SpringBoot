d1c2047ad995dbdfe7c7bc794d8afc6577ecf4d7
GabrielBergamim <gabriel.bergamim@outlook.com>
2019-09-14 01:40:42 -0300

    JWT autenticacao e autorizacao

--- cursomc/src/main/java/com/gabrielbergamim/cursomc/config/SecurityConfig.java
+++ cursomc/src/main/java/com/gabrielbergamim/cursomc/config/SecurityConfig.java
@@ -2,15 +2,20 @@ package com.gabrielbergamim.cursomc.config;
 
 import java.util.Arrays;
 
+import com.gabrielbergamim.cursomc.security.JWTAuthenticationFilter;
+import com.gabrielbergamim.cursomc.security.JWTAuthorizationFilter;
+import com.gabrielbergamim.cursomc.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -24,13 +29,20 @@ public class SecurityConfig extends  WebSecurityConfigurerAdapter {
 	@Autowired
     private Environment env;
 
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
 	private static final String[] PUBLIC_MATCHERS = {
 			"/h2-console/**"
 	};
 
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
-			"/categorias/**"
+			"/categorias/**",
+			"/clientes/**"
 	};
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -44,6 +56,8 @@ public class SecurityConfig extends  WebSecurityConfigurerAdapter {
 		 	.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
@@ -52,6 +66,11 @@ public class SecurityConfig extends  WebSecurityConfigurerAdapter {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 	
 	@Bean
--- cursomc/src/main/java/com/gabrielbergamim/cursomc/services/DbService.java
+++ cursomc/src/main/java/com/gabrielbergamim/cursomc/services/DbService.java
@@ -130,12 +130,13 @@ public class DbService {
 
         Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
         Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+        Endereco e3 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli2, c2);
 
         cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-        cli2.getEnderecos().addAll(Arrays.asList(e2));
+        cli2.getEnderecos().addAll(Arrays.asList(e2, e3));
 
-        clienteRepository.saveAll(Arrays.asList(cli2));
-        enderecoRepository.saveAll(Arrays.asList(e1, e2));
+        clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+        enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
